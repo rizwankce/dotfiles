4c643665c2a86591d1859945d204a6626f03eb78
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2025-01-01 15:02:35 +0100

    Add vim configs

---
+++ bin/vim-setup.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+BASE_DIR="$HOME/.vim"
+PLUGIN_DIR="$BASE_DIR/bundle"
+COLORS_DIR="$BASE_DIR/colors"
+AUTOLOAD_DIR="$BASE_DIR/autoload"
+PLUGINS=(
+  "kien/ctrlp.vim"
+  "vim-airline/vim-airline"
+  "vim-airline/vim-airline-themes"
+  "scrooloose/nerdtree"
+  "tpope/vim-pathogen"
+  "terryma/vim-multiple-cursors"
+  "airblade/vim-gitgutter"
+  "leafgarland/typescript-vim"
+  "bigfish/vim-js-context-coloring"
+  "neoclide/vim-jsx-improve"
+  "posva/vim-vue"
+  "Quramy/vim-js-pretty-template"
+)
+
+mkdir -p $PLUGIN_DIR
+mkdir -p $COLORS_DIR
+mkdir -p $AUTOLOAD_DIR
+
+wget https://raw.githubusercontent.com/rizwankce/Dotfiles/master/.vimrc -O $HOME/.vimrc
+wget https://raw.githubusercontent.com/dracula/vim/master/colors/dracula.vim -O $COLORS_DIR/dracula.vim 
+
+for PLUGIN in ${PLUGINS[@]}; do
+   DIRNAME="$(basename $PLUGIN)"
+   git clone https://github.com/$PLUGIN.git $PLUGIN_DIR/$DIRNAME
+done
+
+cp $PLUGIN_DIR/vim-pathogen/autoload/pathogen.vim $AUTOLOAD_DIR/pathogen.vim
+
+echo "vim setup done <3"
